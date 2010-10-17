cf9c17eb19f6b474c0aa19a8242d6e5d63eb04e6
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-10-17 12:56:18 +0200

    fix indentation

--- lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Dell.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Dell.pm
@@ -4,22 +4,22 @@ use strict;
 use warnings;
 
 sub discovery {
-   my $empty       = shift;
-   my $description = shift;
-   my $session     = shift;
+    my $empty       = shift;
+    my $description = shift;
+    my $session     = shift;
 
-   # for Switch
+    # for Switch
 
-   if ($description =~ m/^Ethernet Switch$/ ) {
-      my $description_new = $session->snmpGet({
-                        oid => '.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0',
-                        up  => 1,
-                     });
-      if (($description_new ne "null") && ($description_new ne "No response from remote host")) {
-         $description = $description_new;
-      }
-   }
-   return $description;
+    if ($description =~ m/^Ethernet Switch$/ ) {
+        my $description_new = $session->snmpGet({
+            oid => '.1.3.6.1.4.1.674.10895.3000.1.2.100.1.0',
+            up  => 1,
+        });
+        if (($description_new ne "null") && ($description_new ne "No response from remote host")) {
+            $description = $description_new;
+        }
+    }
+    return $description;
 }
 
 1;
--- lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
+++ lib/FusionInventory/Agent/Task/NetDiscovery/Manufacturer/Wyse.pm
@@ -4,28 +4,28 @@ use strict;
 use warnings;
 
 sub discovery {
-   my $empty       = shift;
-   my $description = shift;
-   my $session     = shift;
+    my $empty       = shift;
+    my $description = shift;
+    my $session     = shift;
 
-   if ($description =~ m/Linux/) {
+    if ($description =~ m/Linux/) {
 
-      my $description_new = $session->snmpGet({
-                        oid => '.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1',
-                        up  => 1,
-                     });
-      if ($description_new ne "null") {
+        my $description_new = $session->snmpGet({
+            oid => '.1.3.6.1.4.1.714.1.2.5.6.1.2.1.6.1',
+            up  => 1,
+        });
+        if ($description_new ne "null") {
 
-         $description_new =~ s/^"//;
-         $description_new =~ s/"$//;
-         $description = "Wyse ".$description_new;
-      }
-   }
+            $description_new =~ s/^"//;
+            $description_new =~ s/"$//;
+            $description = "Wyse ".$description_new;
+        }
+    }
 
-   # OR ($description{'.1.3.6.1.2.1.1.1.0'} =~ m/Windows/))
-   # In other oid for Windows
+    # OR ($description{'.1.3.6.1.2.1.1.1.0'} =~ m/Windows/))
+    # In other oid for Windows
 
-   return $description;
+    return $description;
 }
 
 1;
